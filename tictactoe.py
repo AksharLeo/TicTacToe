e072d7f2eaff625a0f22e974ff27d484b71e212e
AksharLeo <aksharthirunahari@gmail.com>
2023-05-14 01:29:57 +0530

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,84 @@
+def printBoard(board):
+    print(f'| {board[0]} | {board[1]} | {board[2]} |')
+    print(f'| {board[3]} | {board[4]} | {board[5]} |')
+    print(f'| {board[6]} | {board[7]} | {board[8]} |')
+    print('-----------------------')
+
+
+def gameWonHorizontal(board):
+    if board[0] == board[1] == board[2] and board[0] != '-':
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != '-':
+        return True
+    if board[6] == board[7] == board[8] and board[6] != '-':
+        return True
+
+
+def gameWonVertical(board):
+    if board[0] == board[3] == board[6] and board[0] != '-':
+        return True
+    if board[1] == board[4] == board[7] and board[1] != '-':
+        return True
+    if board[2] == board[5] == board[8] and board[2] != '-':
+        return True
+
+
+def gameWonDiagonal(board):
+    if board[0] == board[4] == board[8] and board[0] != '-':
+        return True
+    if board[6] == board[4] == board[2] and board[6] != '-':
+        return True
+
+
+def gameWon(board):
+    if gameWonDiagonal(board) or gameWonHorizontal(board) or gameWonVertical(board):
+        return True
+    else:
+        return False
+
+
+def gameTie(board):
+    if not gameWon(board):
+        if '-' not in board:
+            return True
+        else:
+            return False
+
+def gameRunning():
+    if not (gameWon(board) or gameTie(board)):
+        return True
+    else:
+        return False
+
+
+def currentPlayer():
+    global player
+    if player == 'X':
+        player = 'O'
+    else:
+        player = 'X'
+    return player
+
+player = ''
+turn = 'X'
+board = ['-', '-', '-',
+        '-', '-', '-',
+        '-', '-', '-']
+
+while gameRunning():
+    printBoard(board)
+    box = int(input(f'{turn} select a box between 1-9: '))
+    if board[box - 1] == '-':
+        board[box - 1] = currentPlayer()
+        turn = 'X' if turn == 'O' else 'O'
+    else:
+        print(f'{box} is already Occupied, choose a different box')
+        continue
+
+printBoard(board)
+
+if gameTie(board):
+    print('Its a draw')
+elif gameWon(board):
+    print(f'{player} Won')
+
